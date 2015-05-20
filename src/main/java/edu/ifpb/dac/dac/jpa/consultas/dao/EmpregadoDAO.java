65813351d5df7c2ce14637d3609497554bba3506
emanuelbatista <emanuelbatista2011@gmail.com>
2015-05-20 07:47:05 -0300

    adicionando funcionalidades iniciais

---
+++ src/main/java/edu/ifpb/dac/dac/jpa/consultas/dao/EmpregadoDAO.java
@@ -0,0 +1,14 @@
+
+package edu.ifpb.dac.dac.jpa.consultas.dao;
+
+import edu.ifpb.dac.dac.jpa.consultas.entidades.Empregado;
+
+/**
+ *
+ * @author Emanuel Batista da Silva Filho
+ */
+public class EmpregadoDAO extends DAOJPA<Empregado>{
+
+    
+
+}
